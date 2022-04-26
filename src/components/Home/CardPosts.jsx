d963010410b071bd3b2a7fed907b68fe66fafe79
christopherdongo <chris_d26@hotmail.com>
2022-04-26 07:42:47 -0500

    update src card

--- src/components/Home/CardPosts.jsx
+++ src/components/Home/CardPosts.jsx
@@ -1,14 +1,8 @@
 import React from 'react'
 import avatar from '../../assets/images/avatar.png'
-import articlephoto from '../../assets/images/Rectangle5.png'
-
-const styles = 'linear-gradient(180deg, rgba(47, 24, 147, 0.0001) 0%, rgba(0, 0, 0, 0.5) 100%)';
 export const CardPosts = ({title, desc,social}) => {
   return (
-    <article className='posts__card' style={{
-
-      backgroundImage:`${styles}, url(${articlephoto})`
-    }}>
+    <article className='posts__card'>
     
       <div className='posts__card__body'>
       <img alt="title" src={avatar} />
